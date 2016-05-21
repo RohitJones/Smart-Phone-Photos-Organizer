a702bf4089e9127f06964f3f793c3aa566dc4749
Rohit Jones <rohitjones21@gmail.com>
2016-05-21 15:11:45 +0530

    final program

--- makefolders.py
+++ makefolders.py
@@ -1,5 +1,29 @@
 import os
 import shutil
+Months = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"
+}
 
-Working_Dir = "C:\\Users\\Jones\\Documents\\Testing area\\"
+Working_Dir = "D:\\Mummy Phone Backup\\Camera\\New folder\\"
 
+Days = {}
+for each in os.listdir(Working_Dir):
+    Date = each[2:10]
+
+    if Date not in Days:
+        Days[Date] = True
+        Actual_Folder = Date[:4] + " (" + Date[4:6] + ") " + Months[Date[4:6]] + " " + Date[6:]
+        os.mkdir(Working_Dir+Actual_Folder)
+
+    shutil.move(Working_Dir+each, Working_Dir+Actual_Folder+"\\"+each)
